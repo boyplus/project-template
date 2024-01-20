f80e833efe3c462739d8200c1fbb174b4af659b8
boyplus <ict224bj@gmail.com>
2024-01-20 15:41:17 +0700

    init router and path alias

--- src/ProjectTemplate.ClientSide/src/main.tsx
+++ src/ProjectTemplate.ClientSide/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Component
+import HomePage from '@/routes/HomePage.tsx'
+
+// Css
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
