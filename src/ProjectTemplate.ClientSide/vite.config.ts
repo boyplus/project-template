e5a8375af4ff2f158ad7585658b4ac1b535a2c2d
boyplus <ict224bj@gmail.com>
2024-01-20 15:43:01 +0700

    add types/node

--- src/ProjectTemplate.ClientSide/vite.config.ts
+++ src/ProjectTemplate.ClientSide/vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
-import url from 'url';
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   plugins: [react()],
   server: {
